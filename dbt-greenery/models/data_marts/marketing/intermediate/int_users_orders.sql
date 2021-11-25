11db422e3249ba23ee7cd0028c5d81570ff947f4
Joshua Smart Olufemi <regaljosh1@gmail.com>
2021-11-25 19:10:31 +0000

    feat: sync project from gitpod

--- dbt-greenery/models/data_marts/marketing/intermediate/int_users_orders.sql
+++ dbt-greenery/models/data_marts/marketing/intermediate/int_users_orders.sql
@@ -11,13 +11,16 @@ with int_users_orders as (
     sum(orders.order_cost) as sum_order_cost,
     sum(orders.shipping_cost) as sum_shipping_cost,
     sum(orders.revenue) as sum_revenue,
-    sum(order_items.order_quantity) as sum_order_quantity
+    sum(order_items.order_quantity) as sum_order_quantity,
+    min(order_items.order_quantity) as minimum_order_quantity,
+    max(order_items.order_quantity) as maximum_order_quantity,
+    date_part('day',orders.estimated_delivery_at :: timestamp - orders.delivered_at:: timestamp) * 24+ date_part('hour',orders.estimated_delivery_at :: timestamp - orders.delivered_at:: timestamp) as hours_btw_estimated_delivery_and_actual_delivery
     FROM {{ref('stg_users')}} as users
     LEFT JOIN {{ref('stg_orders')}} as orders
     ON users.user_id = orders.user_id
     LEFT JOIN {{ref('stg_order_items')}} as order_items
     ON orders.order_id = order_items.order_id
-    GROUP BY 1
+    GROUP BY 1,8
 )
 
 select * from int_users_orders
--- dbt-greenery/models/data_marts/product/intermediate/int_page_views.sql
+++ dbt-greenery/models/data_marts/product/intermediate/int_page_views.sql
@@ -5,16 +5,15 @@
 }}
 with int_page_views as (
     select 
-    to_char(created_at,'Month') as event_month,
-    session_id as views,
-    count(user_id) as no_of_viewers,
+    to_char(created_at,'Month') as Event_Month,
+    event_id,
+    user_id,
+    session_id,
     count(session_id) over (partition by to_char(created_at,'Month')) as views_per_month
     from {{ref('stg_events')}}
-    group by 1,2
+    where event_type = 'page_view'  
 )
 
 select 
-event_month,
-no_of_viewers,
-views_per_month
+*
 from int_page_views
