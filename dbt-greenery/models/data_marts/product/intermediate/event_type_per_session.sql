bd5e18164d274de44e532547bc521d6643c71be6
Joshua Smart Olufemi <regaljosh1@gmail.com>
2021-12-04 20:02:28 +0000

    feat: sync project from gitpod

--- dbt-greenery/models/data_marts/product/intermediate/event_type_per_session.sql
+++ dbt-greenery/models/data_marts/product/intermediate/event_type_per_session.sql
@@ -9,10 +9,10 @@ with event_types_per_session as(
 )
 
 select 
-event_type,
+distinct(event_type) as unique_event_types,
 count(distinct (session_id)) as unique_sessions
 {% for event_type in event_types %}
-,case when event_type = '{{event_type}}' then event_type end as "{{event_type}}s"
+,sum(case when event_type = '{{event_type}}' then 1 else 0 end) as no_of_"{{event_type}}s"
 {% endfor %}
 from event_types_per_session
 group by 1
